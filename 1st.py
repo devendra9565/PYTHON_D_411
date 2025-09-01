d08b9e09209b7eddaa6bba7bc4259928bcf1b6cb
devendra9565 <code.for.dev12@gmail.com>
2025-09-01 15:26:38 +0530

    Python

--- 1st.py
+++ 1st.py
@@ -1,2 +1,21 @@
-a=7
+a=5
 print(a)
+
+x="Zara"
+y= 10
+z= 10.10
+print(type(x))
+print(type(y))
+print(type(z))
+
+x=str(10)
+y=int(10)
+z=float(10)
+print("x =", x )
+print("y =", y )
+print("z =", z )
+
+age = 39
+Age = 78
+print("age =", age )
+print("Age =", Age )
