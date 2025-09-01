9cea53a4319683e4d2e66e275501eb7d19832c63
devendra9565 <code.for.dev12@gmail.com>
2025-09-01 15:35:31 +0530

    Strings, Python Booleans

---
+++ 4th.py
@@ -0,0 +1,27 @@
+a = "Hello"
+b = "World" 
+c = a + b
+print(c)
+c = a + " " + b
+print(c)
+
+age = 19
+a = f"My age is {age}" 
+print(a)
+
+value = 36
+a = f"The value is {value}"
+print(a)
+a = f"The value is {value:.3f}"
+print(a)
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a=64
+b=87
+if b>a:
+    print("b is greater")
+else:
+    print ("a is greater")
