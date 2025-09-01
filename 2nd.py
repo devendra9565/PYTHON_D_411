c2625706935568fed10fa564369311ec47391a94
devendra9565 <code.for.dev12@gmail.com>
2025-09-01 15:29:46 +0530

    Python Variables

---
+++ 2nd.py
@@ -0,0 +1,22 @@
+a=b=c=10
+print(a,b,c)
+
+a,b,c = 5,10,15
+print(a,b,c)
+
+colors = ["red","blue","green"]
+a,b,c = colors
+print(a)
+print(b)
+print(c)
+
+a = "Focus"
+b = "on"
+c = "yourself"
+print(a + b + c)
+print(a, b, c)
+
+x = "calm"
+def myfunc():
+    print("Keep yourself " + x)
+myfunc()
