00a1f5240552d6a0dc78a8a92d0559acf505f55a
devendra9565 <code.for.dev12@gmail.com>
2025-09-01 15:32:36 +0530

    Strings in Python

---
+++ 3rd.py
@@ -0,0 +1,22 @@
+print('hello')
+print("Hello")
+
+a = "Hello,World!"
+print(a[1])
+print(a[8])
+print(len(a))
+
+a = "Opportunities don't happen, you create them"
+print("Opportunities" in a)
+print("Threat" not in a)
+
+x = "Hello,World!"
+print(x[2:7])
+print(x[:7])
+print(x[2:])
+print(x[-7:-2])
+print(x.upper())
+print(x.lower())
+print(x.strip())
+print(x.replace("W","M"))
+print(x.split(","))
